25ba865a9bb6f750ee0796571106d43439ac13d9
IhabBendidi <ihabnobendidi@gmail.com>
2021-01-12 07:53:27 +0100

    added basic front end

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import flask
 import rdflib
 from flask import request
+from flask import render_template
 
 app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "data"
 app.config["DEBUG"] = True
 
 g = rdflib.Graph()
-#g.parse("data/pub-transportation.owl.rdf")
 
 g.parse("data/transit_ontology.owl")
 
@@ -60,7 +60,6 @@ def individu_transports():
 
 @app.route('/api/statistics', methods=['GET'])
 def day_statistics():
-    # TODO : Add filter about days
     moyen = request.args.get('moyen')
     if moyen == '':
         code = 404
@@ -90,7 +89,6 @@ def day_statistics():
 
 @app.route('/api/pollution', methods=['GET'])
 def get_pollution():
-    # TODO : Add filter about days
     moyen = request.args.get('moyen')
     if moyen == '':
         code = 404
@@ -112,6 +110,146 @@ def get_pollution():
             code = 503
             response = None
     return {"code":code,"response":response}
+
+
+
+
+
+def get_pollution(moyen):
+    if moyen == '':
+        code = 404
+        response = None
+    else :
+        code = 200
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                                PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                                PREFIX ex: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                                SELECT ?pollution_level ?type
+                                    WHERE { ?type rdf:type ex:""" + moyen + """.
+                                    ?type ex:pollution_level ?pollution_level.}""")
+            response = str(list(qres)[0].asdict()['pollution_level'])
+        except :
+            code = 503
+            response = None
+    return {"code":code,"response":response}
+
+
+def day_statistics(moyen):
+    if moyen == '':
+        code = 404
+        response = None
+    else :
+        code = 200
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                            PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                            SELECT ?personne
+                            WHERE {
+                                        ?personne rdf:type xd:Person.
+                                        ?personne xd:commutewith xd:""" + moyen + """.
+                            }""")
+            stat = len(list(qres))
+            if stat == 0 :
+                response = []
+                response.append({"moyen":moyen,"statistic":stat})
+                qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                            PREFIX ex: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                            SELECT ?pollution_level ?type
+                                WHERE { ?type rdf:type ex:"""+moyen+""".
+                                ?type ex:pollution_level ?pollution_level}""")
+                for row in qres:
+                    moyen = str(row.asdict()['type']).split("#")[1]
+                    qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                                    PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                                    SELECT ?personne
+                                    WHERE {
+                                                ?personne rdf:type xd:Person.
+                                                ?personne xd:commutewith xd:""" + moyen + """.
+                                    }""")
+                    stat = len(list(qres))
+                    response.append({"moyen":moyen,"statistic":stat})
+
+        except :
+            code = 503
+            response = None
+    return {"code":code,"response":response}
+
+
+
+def individu_transports(individu):
+    #TODO : GET DATA OF EACH TRANSPORT
+    code = 200
+    if individu == '':
+        code = 404
+        transports = None
+    else :
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                            PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                            SELECT ?object
+                                WHERE { xd:""" + individu + """ xd:commutewith ?object}""")
+            transports = [x[0].n3().split("#")[1].split('>')[0] for x in list(qres)]
+        except :
+            code = 503
+            transports = None
+    return {"code":code,"response":transports}
+
+
+@app.route('/', methods = ['GET'])
+def home_page():
+    output = moyen_transports()
+    individus = ['Olaf','Marie','Alexis']
+    if output['code'] == 200:
+        return render_template('index.html' , transports = output["response"],individus = individus)
+    elif output['code'] == 404:
+        return render_template('404.html')
+    else:
+        return render_template('503.html')
+
+@app.route('/transport/<title>', methods = ['GET'])
+def template_transport(title):
+    output = get_pollution(title)
+    statistics = day_statistics(title)
+    if output['code'] == 200 :
+        pollution = output['response']
+    else :
+        pollution = str(0)
+    print(statistics)
+    if statistics['code'] == 200 :
+        response = statistics['response']
+    else :
+        response = []
+    return render_template('template_transport.html', statistics = response ,pollution = pollution , title = title)
+
+
+@app.route('/individus/<individu>', methods = ['GET'])
+def template_individu(individu):
+    output = individu_transports(individu)
+    if output['code'] == 200 :
+        transports = output['response']
+    else :
+        transports = []
+    return render_template('template_individus.html' ,transports = transports , individu = individu)
 
 
 app.run() #host= '0.0.0.0')
