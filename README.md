2bcc30ac5711695c88f3a0edd20670006846d825
IhabBendidi <ihabnobendidi@gmail.com>
2021-01-12 06:02:45 +0100

    added pollution api

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 curl -X GET 'http://127.0.0.1:5000/api/moyen_transports'
 curl -X GET 'http://127.0.0.1:5000/api/individu_transports?individu=Alexis'
-curl -X GET 'http://127.0.0.1:5000/api/statistics'
+curl -X GET 'http://127.0.0.1:5000/api/statistics?moyen=Uber'
+curl -X GET 'http://127.0.0.1:5000/api/pollution?moyen=bus'
--- app.py
+++ app.py
@@ -14,51 +14,104 @@ g.parse("data/transit_ontology.owl")
 
 @app.route('/api/moyen_transports', methods=['GET'])
 def moyen_transports():
-    qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-                    PREFIX ex: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
-                    SELECT ?subject
-                    	WHERE { ?subject rdfs:subClassOf ex:moyen_transports}""")
-    transports = [x[0].n3().split("#")[1].split('>')[0] for x in list(qres)]
-    return {"data":transports}
+    code = 200
+    try :
+        qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                        PREFIX ex: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+                        SELECT ?subject
+                        	WHERE { ?subject rdfs:subClassOf ex:moyen_transports}""")
+        if len(list(qres))==0:
+            code = 200
+            transports = []
+        else :
+            transports = [x[0].n3().split("#")[1].split('>')[0] for x in list(qres)]
+    except :
+        transports = None
+        code = 503
+    return {"code":code,"response":transports}
 
 
 @app.route('/api/individu_transports', methods=['GET'])
 def individu_transports():
+    #TODO : GET DATA OF EACH TRANSPORT
+    code = 200
     individu = request.args.get('individu')
     if individu == '':
-        return {"data":'Please input the correct name of a person'}
-    qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+        code = 404
+        transports = None
+    else :
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                            PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
 
-        SELECT ?object
-            WHERE { xd:""" + individu + """ xd:commutewith ?object}""")
-    transports = [x[0].n3().split("#")[1].split('>')[0] for x in list(qres)]
-    #TODO : GET DATA OF EACH TRANSPORT
-    return {"data":transports}
+                            SELECT ?object
+                                WHERE { xd:""" + individu + """ xd:commutewith ?object}""")
+            transports = [x[0].n3().split("#")[1].split('>')[0] for x in list(qres)]
+        except :
+            code = 503
+            transports = None
+    return {"code":code,"response":transports}
 
 @app.route('/api/statistics', methods=['GET'])
 def day_statistics():
-    qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-                    PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
-
-                    SELECT ?personne
-                    WHERE {
-                                ?personne rdf:type xd:Person.
-                                ?personne xd:commutewith xd:Uber.
-                    }""")
     # TODO : Add filter about days
-    return {"data":len(list(qres))}
+    moyen = request.args.get('moyen')
+    if moyen == '':
+        code = 404
+        stat = None
+    else :
+        code = 200
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                            PREFIX xd: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                            SELECT ?personne
+                            WHERE {
+                                        ?personne rdf:type xd:Person.
+                                        ?personne xd:commutewith xd:""" + moyen + """.
+                            }""")
+            stat = len(list(qres))
+        except :
+            code = 503
+            stat = None
+    return {"code":code,"response":stat}
 
 
+
+
+@app.route('/api/pollution', methods=['GET'])
+def get_pollution():
+    # TODO : Add filter about days
+    moyen = request.args.get('moyen')
+    if moyen == '':
+        code = 404
+        response = None
+    else :
+        code = 200
+        try :
+            qres = g.query("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                                PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                                PREFIX ex: <http://www.semanticweb.org/clementsiegrist/ontologies/2021/0/pub-transportation#>
+
+                                SELECT ?pollution_level ?type
+                                    WHERE { ?type rdf:type ex:""" + moyen + """.
+                                    ?type ex:pollution_level ?pollution_level.}""")
+            response = str(list(qres)[0].asdict()['pollution_level'])
+        except :
+            code = 503
+            response = None
+    return {"code":code,"response":response}
 
 
 app.run() #host= '0.0.0.0')
